34c5418ccf7720ac74f8831b151a8920a357f2d1
Ana Cunha <1771610+anacunha@users.noreply.github.com>
2021-06-07 17:44:47 -0300

    Add secrets placeholder file

---
+++ module-1/pub-sub-mqtt/secrets.h
@@ -0,0 +1,27 @@
+#ifndef secrets_h
+#define secrets_h
+
+const char AWS_IOT_ENDPOINT[] = "<YOUR ENDPOINT HERE>";
+
+// Amazon Root CA 1
+static const char AWS_CERT_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+<COPY AND PASTE HERE>
+-----END CERTIFICATE-----
+)EOF";
+
+// Device Certificate
+static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
+-----BEGIN CERTIFICATE-----
+<COPY AND PASTE HERE>
+-----END CERTIFICATE-----
+)KEY";
+
+// Device Private Key
+static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
+-----BEGIN RSA PRIVATE KEY-----
+<COPY AND PASTE HERE>
+-----END RSA PRIVATE KEY-----
+)KEY";
+
+#endif
